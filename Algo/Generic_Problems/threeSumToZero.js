772edf24c1180855bd17d7d2f3baeae4144ee762
Mahmoud Elbasuony <mahmoud.elbasuony123@gmail.com>
2021-07-14 22:13:06 +0200

    modify 3 sum

--- Algo/Generic_Problems/threeSumToZero.js
+++ Algo/Generic_Problems/threeSumToZero.js
@@ -1,21 +1,26 @@
-function threeSum(arr) {
-  let exists = false;
-
+var threeSum = function (arr) {
+  if (arr.length < 3) return [];
+  let result = new Set();
+  arr.sort((a,b)=> a - b);
   const checkRecursive = (subarr = arr, sum = 0, level = 0, current = []) => {
-    if (level === 3 && sum === 0) {
-      exists = true;
+    if (level === 3) {
+      if (sum === 0) {
+        const sub_result = [...current];
+        sub_result.sort((a,b)=> a- b);
+        result.add(sub_result.join(','));
+      }
       return;
     }
-    for (let element of subarr) {
+    for (let i = 0; i < subarr.length; i++) {
+      const element = subarr[i];
       current.push(element);
-      checkRecursive(subarr.filter((e) => !current.some((x) => x == e)), sum + element, level + 1, current);
+      checkRecursive(subarr.slice(i + 1), sum + element, level + 1, current);
       current.pop();
-      if (exists) break;
     }
   };
 
   checkRecursive();
 
-  return exists;
-}
-console.log(threeSum([1, 1, -8, -2]));
+  return [...result].map(r => r.split(',').map(Number));
+};
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
